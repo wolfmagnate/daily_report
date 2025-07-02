4ecddcef6edeeb83a90eb669984b93d60195b8b0
進捗ゼミ <47470998+wolfmagnate@users.noreply.github.com>
2025-07-02 09:21:53 +0900

    use netlify

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,14 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   // SSRを有効化
   output: 'server',
+
   site: isProd ? 'https://your-production-domain.com' : 'http://localhost:4321',
   integrations: [react()],
-  
+
   vite: {
     css: {
       preprocessorOptions: {
@@ -19,5 +22,7 @@ export default defineConfig({
         },
       },
     },
-  }
+  },
+
+  adapter: netlify()
 });
